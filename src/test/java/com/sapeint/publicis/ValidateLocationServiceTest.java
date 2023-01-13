9fa36be74c38d0ff8196800897f54ada5707bea4
Deepankar Saxena <deepankarsaxena1403@gmail.com>
2023-01-13 15:28:24 +0530

    Update ValidateLocationServiceTest.java

--- src/test/java/com/sapeint/publicis/ValidateLocationServiceTest.java
+++ src/test/java/com/sapeint/publicis/ValidateLocationServiceTest.java
@@ -1,23 +1,23 @@
-package com.sapeint.publicis;
+// package com.sapeint.publicis;
 
-import com.sapeint.publicis.services.interfaces.IValidateLocationService;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+// import com.sapeint.publicis.services.interfaces.IValidateLocationService;
+// import org.junit.Assert;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class ValidateLocationServiceTest {
+// @SpringBootTest
+// @TestPropertySource(locations = "classpath:application-test.properties")
+// public class ValidateLocationServiceTest {
 
-    @Autowired
-    private IValidateLocationService validateLocationService;
+//     @Autowired
+//     private IValidateLocationService validateLocationService;
 
-    @Test
-    public void isLocationValidTest() {
+//     @Test
+//     public void isLocationValidTest() {
 
-        Boolean result = validateLocationService.isLocationValid(10,20);
-        Assert.assertEquals(true,result);
-    }
-}
+//         Boolean result = validateLocationService.isLocationValid(10,20);
+//         Assert.assertEquals(true,result);
+//     }
+// }
